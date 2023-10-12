e6d4a86f479f017ca04d89828df7c2621854c842
harish <tvharish606@gmail.com>
2023-10-12 17:20:49 +0530

    uploaded_playo

---
+++ src/test/java/Playo_Base_Package/Base_Playo_class.java
@@ -0,0 +1,8 @@
+package Playo_Base_Package;
+
+public class Base_Playo_class
+{
+	
+	
+	
+}
---
+++ src/test/java/Test_Playo_Package/Test_scripts_Playo.java
@@ -0,0 +1,8 @@
+package Test_Playo_Package;
+
+import Playo_Base_Package.Base_Playo_class;
+
+public class Test_scripts_Playo extends Base_Playo_class
+{
+
+}
